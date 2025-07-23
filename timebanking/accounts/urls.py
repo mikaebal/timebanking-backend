045bd4c70237bd0d29c73fb39796fc57bc11217e
dehuihu <dehuihu@gmail.com>
2025-07-23 07:20:30 -0700

    Adds user registration

--- timebanking/accounts/urls.py
+++ timebanking/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import RegisterView
+from .views import register_view
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
+    path('register/', register_view, name='register'),
 ]
--- timebanking/accounts/views.py
+++ timebanking/accounts/views.py
@@ -1,7 +1,11 @@
-from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .serializers import RegisterSerializer
-from .models import CustomUser
 
-class RegisterView(generics.CreateAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = RegisterSerializer
+@api_view(['POST'])
+def register_view(request):
+    serializer = RegisterSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"message": "User registered successfully!"}, status=201)
+    return Response(serializer.errors, status=400)
