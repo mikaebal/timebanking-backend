2c176969e3fc31221030d528f86df8fb82d65b92
dehuihu <dehuihu@gmail.com>
2025-07-24 07:49:44 -0700

    Integrates login with api/token-auth

--- timebanking/accounts/urls.py
+++ timebanking/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import profile_view, register_view, login_view
+from .views import profile_view, register_view
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('register/', register_view, name='register'),
-    path('login/', login_view, name="login"),
+    path('login/', obtain_auth_token),
     path('profile/', profile_view, name='profile'),
 ]
--- timebanking/accounts/views.py
+++ timebanking/accounts/views.py
@@ -17,25 +17,6 @@ def register_view(request):
         return Response({"message": "User registered successfully!"}, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# /accounts/login/
-# @csrf_exempt  # Disable CSRF for this view
-@api_view(['POST'])
-@authentication_classes([])  # Disable authentication classes that enforce CSRF
-@permission_classes([])      # Disable permissions for this view
-def login_view(request):
-    username = request.data.get('username')
-    password = request.data.get('password')
-
-    # if not username or not password:
-    #     return Response({'error': 'Username and password are required.'}, status=status.HTTP_400_BAD_REQUEST)
-
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return Response({'message': 'Login successful!'}, status=status.HTTP_200_OK)
-    else:
-        return Response({'error': 'Invalid credentials.'}, status=status.HTTP_401_UNAUTHORIZED)
-
 # /accounts/profile/
 @api_view(['GET', 'PUT', 'PATCH'])
 @permission_classes([IsAuthenticated])
