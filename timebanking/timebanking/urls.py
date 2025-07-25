fa634171dc9c87bd56c396afc996c0ee4aaf95ba
dehuihu <dehuihu@gmail.com>
2025-07-25 12:50:14 -0700

    Adds api doc using drf-yasg but failed due to version incompability

--- timebanking/timebanking/urls.py
+++ timebanking/timebanking/urls.py
@@ -18,9 +18,30 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework.authtoken.views import obtain_auth_token
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.urls import path, re_path
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Timebanking_API",
+        default_version='v1',
+        description="API documentation for timebanking web app",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path('admin/', admin.site.urls),
     path('api/token-auth/', obtain_auth_token, name='api_token_auth'),
     path('accounts/', include('accounts.urls')),
+
+    # Swagger UI
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    
+    # ReDoc UI (optional)
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
