71541c122296b4122a1f32c28527ede7d1db3b68
dehuihu <dehuihu@gmail.com>
2025-07-25 09:36:54 -0700

    Adds CustomUserAdmin to see additional fields in AdminPanel

--- timebanking/accounts/admin.py
+++ timebanking/accounts/admin.py
@@ -5,4 +5,29 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    # Add custom fields to the detail/edit view
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Info", {
+            "fields": (
+                "bio", "skills", "interests",
+                "time_credits", "city", "state", "zip_code"
+            )
+        }),
+    )
+
+    # Add custom fields to the user creation form
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional Info", {
+            "fields": (
+                "bio", "skills", "interests",
+                "time_credits", "city", "state", "zip_code"
+            )
+        }),
+    )
+
+    # Optional: display custom fields in the user list view
+    list_display = (
+        "username", "email", "city", "state", "zip_code", "time_credits", "is_staff"
+    )
+admin.site.register(User, CustomUserAdmin)
