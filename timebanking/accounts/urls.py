ea6c48de20fe45e1dd6fc48ed60ec097e0319aa9
dehuihu <dehuihu@gmail.com>
2025-07-25 15:10:51 -0700

    Converts register and userprofile views to class-based to allow API doc to display parameters properly

--- timebanking/accounts/urls.py
+++ timebanking/accounts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import profile_view, register_view
+from .views import UserProfileView, RegisterView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('register/', register_view, name='register'),
+    path('register/', RegisterView.as_view(), name='register'),
     path('login/', obtain_auth_token),
-    path('profile/', profile_view, name='profile'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
 ]
--- timebanking/accounts/views.py
+++ timebanking/accounts/views.py
@@ -1,33 +1,38 @@
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
-from .serializers import RegisterSerializer
-# from django.contrib.auth import authenticate, login
-# from django.views.decorators.csrf import csrf_exempt
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from .serializers import UserProfileSerializer
+from rest_framework import status
+from .serializers import RegisterSerializer, UserProfileSerializer
 
 # /accounts/register/
-@api_view(['POST'])
-def register_view(request):
-    serializer = RegisterSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response({"message": "User registered successfully!"}, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# convert to class-based views
+class RegisterView(APIView):
+    serializer_class = RegisterSerializer
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "User registered successfully!"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # /accounts/profile/
-@api_view(['GET', 'PUT', 'PATCH'])
-@permission_classes([IsAuthenticated])
-def profile_view(request):
-    user = request.user
-    if request.method == 'GET':
-        serializer = UserProfileSerializer(user)
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserProfileSerializer
+
+    def get(self, request):
+        serializer = self.serializer_class(request.user)
         return Response(serializer.data)
-    elif request.method in ['PUT', 'PATCH']:
-        serializer = UserProfileSerializer(user, data=request.data, partial=True)
+
+    def put(self, request):
+        return self.update_profile(request, partial=False)
+
+    def patch(self, request):
+        return self.update_profile(request, partial=True)
+
+    def update_profile(self, request, partial):
+        serializer = self.serializer_class(request.user, data=request.data, partial=partial)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, status=400)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
